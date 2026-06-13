391cec66ac8052a4124dbb308238e845f67c266b
Moises Henrique <moihgm67@gmail.com>
2026-06-13 02:57:55 -0300

    refactor: remove tags module and its associated components tags

--- src/main/java/com/moisaas/the_moments/tags/domain/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.moisaas.the_moments.tags.domain.repository;
-
-import com.moisaas.the_moments.tags.domain.entities.TagEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends JpaRepository<TagEntity, String> {
-}
