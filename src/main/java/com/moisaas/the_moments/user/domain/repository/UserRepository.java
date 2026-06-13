cb819861a25ac5ed8c277fd14c0733a543199086
Moises Henrique <moihgm67@gmail.com>
2026-06-13 02:58:18 -0300

    refactor: remove user module components

--- src/main/java/com/moisaas/the_moments/user/domain/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.moisaas.the_moments.user.domain.repository;
-
-import com.moisaas.the_moments.user.domain.entities.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-}
