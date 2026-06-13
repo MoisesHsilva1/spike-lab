6c388bc3634f985f7310bcd1240b2da224f5ad39
Moises Henrique <moihgm67@gmail.com>
2026-06-13 02:56:22 -0300

    refactor: remove image upload, stars, and tags from post creation

--- src/main/java/com/moisaas/the_moments/posts/application/usecases/CreatePostUseCase.java
+++ src/main/java/com/moisaas/the_moments/posts/application/usecases/CreatePostUseCase.java
@@ -5,28 +5,21 @@ import com.moisaas.the_moments.posts.application.dtos.PostDto;
 import com.moisaas.the_moments.posts.application.mapper.PostMapper;
 import com.moisaas.the_moments.posts.domain.entities.PostEntity;
 import com.moisaas.the_moments.posts.domain.repository.PostRepository;
-import com.moisaas.the_moments.shared.infrastructure.services.FileStorageService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class CreatePostUseCase {
     private final PostRepository postsRepository;
     private final PostMapper postMapper;
-    private final FileStorageService fileStorageService;
 
     @Transactional
-    public PostDto execute(CreatePostDto post, MultipartFile image)  {
-        String imageKey = fileStorageService.uploadFile(image);
+    public PostDto execute(CreatePostDto post) {
+        PostEntity entity = postMapper.toEntity(post);
 
-        PostEntity entity = postMapper.toEntity(post, imageKey);
-        PostDto response = postMapper.toDto(postsRepository.save(entity));
-
-        response.setImageUrl(fileStorageService.getPresignedUrl(imageKey));
-
-        return response;
+        return postMapper.toDto(postsRepository.save(entity));
     }
 }
